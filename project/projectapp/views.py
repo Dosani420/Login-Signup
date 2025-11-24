7c3894f26c74d677553b0f24f0e1d06c73ff35e5
Dosani420 <muzammil.kalhoro4@gmail.com>
2025-11-24 15:07:12 +0500

    initial commit

---
+++ project/projectapp/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render,redirect,HttpResponse
+from .models import person
+
+# Create your views here.
+
+def index(request):
+
+    return render(request,'index.html')
+
+def signup(request):
+
+    return render(request,'Signup.html')
+
+def register(request):
+    if request.method == "POST":
+        fullname = request.POST.get("fullname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        person1 = person(
+            name=fullname,
+            email=email,
+            password=password
+        )
+        person1.save()
+
+        return redirect('index')
+
+    return render(request, "Signup.html")
+
+def login(request):
+    if request.method == "POST":
+        em = request.POST.get("email")
+        password = request.POST.get("password")
+        person1 = person.objects.all()
+        for p in person1:
+            if p.email ==  em and p.password == password:
+                d= {"name":p.name}
+                return render(request, "home.html",d)
+        else:
+            return HttpResponse("Wrong username or password")
+    else:
+        return render(request, "index.html")
+
